8703d9182c7e2e993ee4d2244ac13a04337d316f
zd <1298698045@qq.com>
2021-09-27 16:53:25 +0800

    添加icon

--- js/app.js
+++ js/app.js
@@ -51,3 +51,57 @@ var mixin = {
         }
     }
 }
+const Modal = 
+Vue.extend({
+    template: `
+        <el-dialog
+        title="试用申请咨询"
+        :visible.sync="dialogVisible"
+        width="440px"
+        :before-close="handleClose">
+        <div class="modalContent">
+            <div class="desc">完善以下信息您可以免费体验我们的产品功能，我们的顾问会在 1个工作日内与您取得联系</div>
+            <el-form ref="form" :rules="rules" :model="modalForm" label-width="80px">
+                <el-form-item required class="form_item" size="mini" label="单位名称" prop="unitName">
+                    <el-input v-model="modalForm.unitName"></el-input>
+                </el-form-item>
+                <el-form-item required class="form_item" size="mini" label="姓名" prop="name">
+                    <el-input v-model="modalForm.name"></el-input>
+                </el-form-item>
+                <el-form-item required class="form_item" size="mini" label="手机号" prop="phone">
+                    <el-input v-model="modalForm.phone"></el-input>
+                </el-form-item>
+            </el-form>
+        </div>
+        <span slot="footer" class="dialog-footer">
+            <el-button class="submitBtn" type="primary" @click="dialogVisible = false">提交申请</el-button>
+        </span>
+    </el-dialog>
+    `,
+    data(){
+        return {
+            dialogVisible:false,
+            modalForm:{
+                unitName: '',
+                name: '',
+                phone: ''
+            },
+            rules:{
+                unitName:[
+                    { required: true, message: '请输入单位名称', trigger: 'blur' }
+                ],
+                name:[
+                    { required: true, message: '请输入姓名', trigger: 'blur' }
+                ],
+                phone:[
+                    { required: true, message: '请输入电话', trigger: 'blur' }
+                ]
+            }
+        }
+    },
+    methods:{
+        handleClose(){
+            this.dialogVisible = false;
+        }
+    }
+})
